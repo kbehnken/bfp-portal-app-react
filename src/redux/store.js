37c84f44f009b6ba7b2df4a97d8931ecef88bac3
Katrina Behnken <katrina.behnken@gmail.com>
2020-01-05 16:59:58 -0700

    start redux store set up

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+
+const rootReducer = combineReducers( {
+
+})
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
