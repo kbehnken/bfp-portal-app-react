25a1ac5c82931015542a15bec4506698bf772a70
Katrina Behnken <katrina.behnken@gmail.com>
2020-01-05 18:24:58 -0700

    add state & axios calls for user auth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+// import Header from './Components/Header';
+import Auth from './Components/Auth';
+import { HashRouter as Router} from "react-router-dom";
+// import routes from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      emailAddress: '',
+      password: ''
+    };
+    this.handleEmailAddressInput = this.handleEmailAddressInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  handleEmailAddressInput(value) {
+    this.setState( { 
+        emailAddress: value
+    });
+}
+  handlePasswordInput(value) {
+      this.setState( {
+          password: value
+      });
+  }
+  updateUser(user) {
+    this.setState({
+      user,
+    });
+  }
+  login() {
+    const { emailAddress, password } = this.state;
+    axios
+    .post('api/auth/login', {emailAddress, password})
+    .then(user => {
+      this.updateUser(user.data);
+    }) 
+    .catch(err => alert(err.response.request.response));
+  }
+  logout() {
+    axios
+    .post('api/auth/logout')
+    .then(() => {
+        this.updateUser({});
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    const { user, emailAddress, password } = this.state;
+    return (
+      <div>
+        {user.emailAddress ?
+          (
+            <Router>
+            <div>
+              <header className="App-header">
+                {/* <Header logoutFn={this.logout} /> */}
+              </header>
+              <div className="dashboard-container">
+                <div className="main-container">
+                  <main>
+                    {/* {routes} */}
+                  </main>
+                </div>
+              </div>
+            </div>
+            </Router>
+          ) :
+          (
+            <div className='main-container'>
+              <div>
+                <main>
+                  <Auth user={user} emailAddress={emailAddress} password={password} handleEmailAddressInputFn={this.handleEmailAddressInput} handlePasswordInputFn={this.handlePasswordInput} updateUserFn={this.updateUser} loginFn={this.login} />
+                </main>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
