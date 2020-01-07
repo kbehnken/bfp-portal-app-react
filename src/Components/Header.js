fe62f24d5a9d7e3f77d7f1de66dedfc7734cd598
Katrina Behnken <katrina.behnken@gmail.com>
2020-01-07 09:37:19 -0700

    add onClick={props.logoutFn} & { AdminRoutes }

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { HashRouter as Router} from "react-router-dom";
+
+import AdminRoutes from "./../AdminRoutes";
+// import CustomerRoutes from "./../CustomerRoutes";
 
 export default function Header(props) {
     return (
+        <Router>
         <div>
+            <header className="App-header">
+                <div>
+                    Beach Family Pools
+                </div>
+                <div>
+                    Reset Password | <span onClick={props.logoutFn}>Logout</span>
+                </div>
+            </header>
             <div>
-                Beach Family Pools
-            </div>
-            <div className='logout' onClick={props.logoutFn}>
-                Reset Password | Logout
+                <main>
+                    { AdminRoutes }
+                </main>
             </div>
         </div>
+        </Router>
     ); 
 }
