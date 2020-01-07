0ef8dd4bf203ed38a6403e3831cc9cb6a0bf42be
Katrina Behnken <katrina.behnken@gmail.com>
2020-01-07 06:57:36 -0700

    add userReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers( {
-
+    users: userReducer
 })
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
