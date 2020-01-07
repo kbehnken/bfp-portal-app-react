78dad33e4fa534cadeb222d942cf44d4e79d7a12
Katrina Behnken <katrina.behnken@gmail.com>
2020-01-07 06:52:36 -0700

    remove addUser

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,43 +1,26 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-    addUser: async (req, res, next) => {
-        const { is_admin, role, first_name, last_name, phone_number, email_address, password } = req.body;
-        const db = req.app.get('db');
-        const result = await db.get_user(email_address);
-        const existingUser = result[0];
-        if(existingUser) {
-            return res.status(409).send('Email is already in use.')
-        }
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(password, salt);
-        const addedUser = await db.add_user(is_admin, role, first_name, last_name, phone_number, email_address, hash);
-        const user = addedUser[0];
-        req.session.user = {
-            id: user.id,
-            email_address: user.email_address
-        };
-        return res.status(200).send(req.session.user)
-    },
+    
     login: async (req, res, next) => {
-        const { email_address, password } = req.body;
-        const foundUser = await req.app.get('db').get_user([email_address]);
+        const { emailAddress, password } = req.body;
+        const foundUser = await req.app.get("db").get_user([emailAddress]);
         const user = foundUser[0];
         if(!user) {
-            return res.status(401).send('User not found. Please register as a new user before logging in.')
+            return res.status(401).send("User not found. Please contact Beach Family Pools for assistance.")
         }
         const isAuthenticated = bcrypt.compareSync(password, user.hash);
         if (!isAuthenticated) {
-            return res.status(401).send('Incorrect password.');
+            return res.status(401).send("Incorrect password.");
         }
         req.session.user = {
             id: user.id,
-            email_address: user.email_address
+            emailAddress: user.email_address
         };
         return res.status(200).send(req.session.user);
     },
     logout: (req, res, next) => {
         req.session.destroy();
-        res.status(200).send('You have been logged out.');
+        res.status(200).send("You have been logged out.");
     }
 };
