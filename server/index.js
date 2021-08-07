8091e3f3398a0009876b138d8bfb43e6255561d4
Katrina Behnken <katrina.behnken@gmail.com>
2021-08-07 13:54:39 -0700

    Update api routes

--- server/index.js
+++ server/index.js
@@ -29,43 +29,46 @@ app.use(
   })
 );
 
-app.post('/api/auth/login', authCtrl.login);
-app.post('/api/auth/logout', authCtrl.logout);
+app.post('/api/v1/auth/login', authCtrl.login);
+app.post('/api/v1/auth/logout', authCtrl.logout);
 
-app.get('/api/users', userCtrl.getUsers);
-app.post('/api/user/add-user', userCtrl.addUser);
-app.put('/api/user/', auth.usersOnly, userCtrl.updateUser);
+app.get('/api/v1/users', userCtrl.getUsers);
+app.post('/api/v1/user/add-user', userCtrl.addUser);
+app.put('/api/v1/user/', auth.usersOnly, userCtrl.updateUser);
 
-app.get('/api/services', auth.usersOnly, servicesCtrl.getServices);
-app.post('/api/service', auth.usersOnly, servicesCtrl.addService);
-app.put('/api/service/', auth.usersOnly, servicesCtrl.updateService);
-app.delete('/api/service/:id', auth.usersOnly, servicesCtrl.deleteService);
+app.get('/api/v1/services', auth.usersOnly, servicesCtrl.getServices);
+app.post('/api/v1/service', auth.usersOnly, servicesCtrl.addService);
+app.put('/api/v1/service/', auth.usersOnly, servicesCtrl.updateService);
+app.delete('/api/v1/service/:id', auth.usersOnly, servicesCtrl.deleteService);
 
-app.get('/api/equipment', auth.usersOnly, equipmentCtrl.getEquipment);
-app.post('/api/equipment', auth.usersOnly, equipmentCtrl.addEquipment);
-app.put('/api/equipment/:id', auth.usersOnly, equipmentCtrl.updateEquipment);
-app.delete('/api/equipment/:id', auth.usersOnly, equipmentCtrl.deleteEquipment);
+app.get('/api/v1/equipment', auth.usersOnly, equipmentCtrl.getEquipment);
+app.post('/api/v1/equipment', auth.usersOnly, equipmentCtrl.addEquipment);
+app.put('/api/v1/equipment/:id', auth.usersOnly, equipmentCtrl.updateEquipment);
+app.delete('/api/v1/equipment/:id', auth.usersOnly, equipmentCtrl.deleteEquipment);
 
-app.get('/api/photos', auth.usersOnly, photosCtrl.getPhotos);
-app.post('/api/photos', auth.usersOnly, photosCtrl.addPhoto);
-// app.put('/api/photos/:id', auth.usersOnly, photosCtrl.updatePhoto);
-app.delete('/api/photos/:id', auth.usersOnly, photosCtrl.deletePhoto);
+app.get('/api/v1/photos', auth.usersOnly, photosCtrl.getPhotos);
+app.post('/api/v1/photos', auth.usersOnly, photosCtrl.addPhoto);
+// app.put('/api/v1/photos/:id', auth.usersOnly, photosCtrl.updatePhoto);
+app.delete('/api/v1/photos/:id', auth.usersOnly, photosCtrl.deletePhoto);
 
-app.get('/api/addresses', auth.usersOnly, addressesCtrl.getUserAddresses);
-app.get('/api/addresses/:id', auth.usersOnly, addressesCtrl.getAddressesById);
-app.post('/api/address', auth.usersOnly, addressesCtrl.addAddress);
-app.put('/api/address/:id', auth.usersOnly, addressesCtrl.updateAddress);
-app.delete('/api/address/:id', auth.usersOnly, addressesCtrl.deleteAddress);
+app.get('/api/v1/addresses', auth.usersOnly, addressesCtrl.getAddresses);
+app.get('/api/v1/addresses/byCustomerId/:id', auth.usersOnly, addressesCtrl.getAddressesByCustomerId);
+app.get('/api/v1/addresses/:id', auth.usersOnly, addressesCtrl.getAddressesById);
+app.post('/api/v1/address', auth.usersOnly, addressesCtrl.addAddress);
+app.put('/api/v1/address/:id', auth.usersOnly, addressesCtrl.updateAddress);
+app.delete('/api/v1/address/:id', auth.usersOnly, addressesCtrl.deleteAddress);
 
-app.get('/api/calls', auth.usersOnly, callsCtrl.getCalls);
-app.post('/api/call', auth.usersOnly, callsCtrl.addCall);
-app.put('/api/call/:id', auth.usersOnly, callsCtrl.updateCall);
-app.delete('/api/call/:id', auth.usersOnly, callsCtrl.deleteCall);
+// app.get('/api/v1/calls', auth.usersOnly, callsCtrl.getCalls);
+// app.get('/api/v1/calls/byCustomer/:id', auth.usersOnly, callsCtrl.getCallsByCustomer);
+// app.get('/api/v1/calls/byAddress/:id', auth.usersOnly, callsCtrl.getCallsByAddress);
+// app.post('/api/v1/call', auth.usersOnly, callsCtrl.addCall);
+// app.put('/api/v1/call/:id', auth.usersOnly, callsCtrl.updateCall);
+// app.delete('/api/v1/call/:id', auth.usersOnly, callsCtrl.deleteCall);
 
-app.get('/api/invoices', auth.usersOnly, invoicesCtrl.getInvoices);
-app.post('/api/invoice', auth.usersOnly, invoicesCtrl.addInvoice);
-app.put('/api/invoice/:id', auth.usersOnly, invoicesCtrl.updateInvoice);
-app.delete('/api/invoice/:id', auth.usersOnly, invoicesCtrl.deleteInvoice);
+app.get('/api/v1/invoices', auth.usersOnly, invoicesCtrl.getInvoices);
+app.post('/api/v1/invoice', auth.usersOnly, invoicesCtrl.addInvoice);
+app.put('/api/v1/invoice/:id', auth.usersOnly, invoicesCtrl.updateInvoice);
+app.delete('/api/v1/invoice/:id', auth.usersOnly, invoicesCtrl.deleteInvoice);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
